fbfb54bf96463718468077ec187d7ddc8196b5a3
SNR <75149094+snr-development02@users.noreply.github.com>
2023-07-27 23:43:24 +0300

    Add files via upload

---
+++ shared/locales.lua
@@ -0,0 +1,8 @@
+locale = {} -- SNR STORE | https://discord.gg/TtHFpf3enK
+
+locale.uyari = "snr-motel Scripttindeki shared/config.lua üzerinden Config.ESXorQBorNewQB seçeneğini doğru giriniz! (esx/qb/newqb)"
+locale.Newmotel = 'Yeni motel odası verildi! Oda numaran: '
+locale.sandik = '[~g~E~w~] - Sandık'
+locale.gardrop = '[~g~E~w~] - Gardrop'
+locale.targetsandik = "Sandığı aç"
+locale.targetgardrop = "Gardıropu aç"
