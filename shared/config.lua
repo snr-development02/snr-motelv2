e3ed2372849390a80deec330c48b109f68b326a1
SNR <75149094+snr-development02@users.noreply.github.com>
2023-08-06 16:42:41 +0300

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,7 +1,7 @@
 Config = {} -- SNR STORE | https://discord.gg/TtHFpf3enK
 
 Config.ESXorQBorNewQB = "esx" -- esx | qb | newqb
-Config.Target = false -- qb gerekir
+Config.Target = false -- need qb or newqb
 
-Config.MotelBlip = vector3(326.747, -208.35, 54.0866) -- Motel Blip Kordinatları
-Config.MotelBlipIsim = "PinkCage Motel"
+Config.MotelBlip = vector3(326.747, -208.35, 54.0866) -- Motel Blip Coords
+Config.MotelBlipIsim = "PinkCage Motel" --Motel Blip Name
